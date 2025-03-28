61995e42239e72b6530bca7e26d00d9f10088794
Khushika <mamidi.khushika2021@vitstudent.ac.in>
2025-03-28 20:02:00 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="jnanasetu",
+    version="0.0.0",
+    author="Khushika",
+    author_email="mamidikhushikareddy@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
